d0415e3d480e724fbd76ab09b6002a23527218ae
Jiang-Nianxi <chen1136284722@foxmail.com>
2025-10-16 10:27:14 +0800

    printf
    学习了占位符，先占住地方，然后用数据填补

---
+++ printf/printf/printf.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	//输出整数
+	printf("整数: %d\n",123);
+	//输出浮点数
+	printf("浮点数: %f\n", 3.14);
+	//输出字符
+	printf("字符: %c\n", 'A');
+	//输出字符串
+	printf("字符串: %s\n", "Helloword");
+	printf("我的名字是：%s","姜\n");
+
+	//练习
+	printf("我的学校是：%s","广科大\n");
+	printf("我的高考分数是：%d\n", 439);
+	return 0;
+}
