ea48a2cfd4d721be76643d49681f8d6c111ce56e
Jiang-Nianxi <chen1136284722@foxmail.com>
2025-10-20 01:39:17 +0800

    各种进制
    其他转10进制
    系数x基数的权次幂相加
    10进制转其他
    一直除基数，余数从下往上

---
+++ jinzhi/jinzhizhuanhuan.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	//二进制
+	int a = 0b10;
+	printf("二进制 0b10 = %d\n", a);
+	//八进制
+	int b = 010;
+	printf("八进制 010 = %d\n", b);
+	//十六进制
+	int c = 0x10;
+	printf("十六进制 0x10 = %d\n", c);
+
+
+	return 0;
+}
