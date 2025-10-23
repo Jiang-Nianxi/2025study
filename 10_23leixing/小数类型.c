3bff877681c661a171d79e3cc52eee1a7e8b6d4b
Jiang-Nianxi <chen1136284722@foxmail.com>
2025-10-24 02:23:31 +0800

    小数整形
    标识符、scanf的应用

---
+++ 10_23leixing/小数类型.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	//float a = 3.14f;
+	//printf("The value of a is: %.2f\n", a);//小数点后保留两位：.2f
+	//double b = 1.78;
+	//printf("The value of b is: %.2lf\n", b);//double类型小数点后保留两位：.2lf
+	//long double c = 3.1415926;
+	//printf("The value of c is: %.4Lf\n", c);//long double类型小数点后保留四位：.4Lf
+	//printf("%zu\n", sizeof(float));
+	//printf("%zu\n", sizeof(a));
+
+	//printf("%zu\n", sizeof(double));
+	//printf("%zu\n", sizeof(b));
+
+	//printf("%zu\n", sizeof(long double));
+	//printf("%zu\n", sizeof(c));
+
+	////char 字符 占用一个字节
+	//char c1 = 'a';
+	//printf("%c\n", c1);
+
+	//char c2 = '1';
+	//printf("%c\n", c2);
+
+	//char c3 = 'A';
+	//printf("%c\n", c3);
+
+	//char c4 = '.';
+	//printf("%c\n", c4);
+
+	//char c5 = '你';
+	//printf("%c\n", c5);
+
+	//标识符的学习
+	//数字、字母、下划线组成
+	//不能以数字开头
+	//不能使用关键字
+	//区分大小写
+	//用英文单词有意义命名，见名知意
+	//变量命名一般使用小写
+	//代码文件名一般使用小写，单词之间使用下划线分隔，可以用数字开头
+
+	//键盘录入：scanf
+	//int a = 0;
+	//printf("请输入一个整数：");
+	//scanf("%d", &a);
+	//printf("你输入的整数是：%d\n", a);
+	//练习：
+	
+	//数据类型 数组名[数组长度] = { 初始化列表 };
+	//1个字母、符号占用1个字节，中文占用2个字节
+	//结束标记1个字节
+	/*char b[20] = { 0 };
+	scanf("%s", &b);
+	printf("名字是：%s\n", b);
+
+	int age = 0;
+	scanf("%d", &age);
+	printf("年龄是：%d\n", age);*/
+
+	
+
+
+
+	return 0;
+}
