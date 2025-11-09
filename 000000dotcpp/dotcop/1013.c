90d9b47d6617559ce396c0165534460b2cc816c5
Jiang-Nianxi <chen1136284722@foxmail.com>
2025-11-10 02:17:49 +0800

    dotcop1013

---
+++ 000000dotcpp/dotcop/1013.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main() {
+//	int a;
+//	int b = 0;
+//	int temp=0;
+//
+//	
+//	scanf("%d", &a);
+//	for (int i = 0; i <= a-1; i++) {
+//		
+//		b = b * 10 + 2;
+//		
+//		temp = temp + b;
+//		
+//
+//	}
+//	printf("%d\n", temp);
+//}
