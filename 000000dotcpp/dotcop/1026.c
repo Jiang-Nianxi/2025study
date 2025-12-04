a9ac6c9440b2febc6159be81169dee1897b80222
Jiang-Nianxi <chen1136284722@foxmail.com>
2025-12-04 23:44:45 +0800

    dotcpp1026
    倒序遍历数组

---
+++ 000000dotcpp/dotcop/1026.c
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//    int arr[10] = { 0 };
+//    int i;
+//    int j;
+//    for (i = 0; i < 10; i++) {
+//        scanf("%d", &arr[i]);
+//    }
+//    // °Ñ j <= 0 ¸Ä³É j >= 0
+//    for (j = 9; j >= 0; j--) {
+//        printf("%d ", arr[j]);
+//    }
+//}
